b751026f761a1792267cc31f9e5847886ede2b10
ali-bahmani <alibahmani0@gmail.com>
2021-06-23 07:04:50 +0430

    register user email

---
+++ app/Mail/registerUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class registerUser extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $name;
+    public $email;
+
+    public function __construct(User $user)
+    {
+        $this->name = $user->name;
+        $this->email = $user->email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->email)
+                    ->view('Mails.registerMail')
+                    ->subject('register successful')
+                    ->with('name' , $this->name);
+    }
+}
---
+++ resources/views/Mails/registerMail.blade.php
@@ -0,0 +1,29 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="box">
+                deear {{$name}} welcome to social
+            </div>
+
+            <div class="box">
+            successful register
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+<style>
+.box{
+    border: 1px solid burlywood;
+    width: 500px;
+    padding: 50px;
+    font-size: 30px;
+    text-align: center;
+    margin: auto;
+        margin-top: auto;
+    margin-top: 50px;
+}
+</style>
